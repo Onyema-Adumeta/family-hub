8249a2760ac1356ef96f06341ee2eb107eea2e88
Onyema Adumeta <badumeta@gmail.com>
2026-06-01 16:54:55 -0700

    fix: set reactNativePath for monorepo EAS build

---
+++ packages/mobile/react-native.config.js
@@ -0,0 +1,10 @@
+﻿const path = require('path');
+
+module.exports = {
+  project: {
+    android: {},
+    ios: {},
+  },
+  dependencies: {},
+  reactNativePath: path.resolve(__dirname, '../../node_modules/react-native'),
+};
